f4cf429d1c4fd52bfd61c9cd25ca81c951247bed
nrepasy <nrepasy@gmail.com>
2021-02-10 11:30:40 -0500

    womp

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 // write your code here
+
+const spiceBlendPhoto = document.getElementById('spice-blend-detail')
+const ingredientContainer = document.getElementsByClassName('ingredients-container')
+
+const spiceBlendIngred = document.querySelector('#ingredients-container')
+
+// ********fetch request**********
+
+window.onload = function getSpiceBlend(){
+    fetch ('http://localhost:3000/spiceblends/1')
+        .then(res => res.json())
+        .then(data => showSpiceBlend(data))
+        
+        }
+        
+        
+     window.onload =   function getSpiceBlendIng(){
+            fetch ('http://localhost:3000/spiceblends/1')
+                .then(res => res.json())
+                .then(data => showSpiceIng(data.ingredients))
+               
+
+                }
+                    
+                
+                
+        
+
+
+
+// *******************functons******************
+
+function showSpiceBlend(data){
+  
+spiceBlendPhoto.innerHTML = `
+<h2>${data.title}</h2>
+<img class= 'detail-image'  src=${data.image} />
+`
+
+
+}
+function showSpiceIng(data){
+    (data.forEach(myfunction));
+    function myfunction(item, index){
+   (item.name)
+    console.log(ingredientContainer)
+    }
+    
+     }
+
+
+   
